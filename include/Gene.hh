9d85e2b1ac46bd85284fac1ab27fb53a9124ba32
Sam von der Dunk <samvonderdunk@hotmail.com>
2019-11-21 10:55:45 +0100

    Genes have an int "expression", no longer a bool "original_five"

--- include/Gene.hh
+++ include/Gene.hh
@@ -8,10 +8,10 @@ class Gene : public Bead {
    int threshold; //Note that a threshold actually represents the lowest value for which a gene becomes active, i.e. if threshold=1, then the actual threshold lies at 0.5 and the gene will be expressed if the activity sums up to 1 or more.
    int activity;  //TF behaviour depends on the TF, i.e. the gene; not the TFBS.
    bool binding_domain[binding_length];
-   bool original_five;
+   int expression;
 
   Gene();//constructor
-  Gene(int t, int th, int act, bool bd_dom[], bool orig);
+  Gene(int t, int th, int act, bool bd_dom[], int expr);
   explicit Gene(const Gene &gene);//copy constructor
   ~Gene();//destructor
 
--- src/Gene.cc
+++ src/Gene.cc
@@ -1,33 +1,37 @@
 #include "Gene.hh"
 #include "Header.hh"
 
-Gene::Gene() : Bead() {
+Gene::Gene() : Bead()
+{
   type = 0;
   threshold = 0;
   activity = 0;
   for(int i=0; i<binding_length; i++) binding_domain[i] = 0;
-  original_five = false;
+  expression = 0;
 }
 
-Gene::Gene(int t, int th, int act, bool bd_dom[], bool orig) : Bead() {
+Gene::Gene(int t, int th, int act, bool bd_dom[], int expr) : Bead()
+{
   type=t;
   threshold=th;
   activity=act;
   for(int i=0; i<binding_length; i++) binding_domain[i] = bd_dom[i];
-  original_five=orig;
+  expression=expr;
 }
 
-Gene::Gene(const Gene &gene) : Bead(gene) {    //See below.. indeed this function copies from the object gene (class Gene; not a pointer or anything!), which you can tell from the fact that members of gene are accessed by dots, i.e. gene.type
+Gene::Gene(const Gene &gene) : Bead(gene)    //See below.. indeed this function copies from the object gene (class Gene; not a pointer or anything!), which you can tell from the fact that members of gene are accessed by dots, i.e. gene.type
+{
   type=gene.type;
   threshold=gene.threshold;
   activity=gene.activity;
   for(int i=0; i<binding_length; i++) binding_domain[i] = gene.binding_domain[i];
-  original_five=false;  //Only if you mutate will you lose this property
+  expression=gene.expression;
 }
 
-Gene::~Gene(){
+Gene::~Gene() {
 }
 
-Bead* Gene::Clone() const {    //This function returns a pointer to a bead (a gene in this case). It creates a pointer to a Gene via p_Gene=new Gene(); this is how you can initialise a new pointer to the type Gene. However, it does not call the constructor, because the brackets are not empty. Instead it calls the copy constructor (see above), which by rule requires the reference to a Gene class object (the ampersand does not indicate the "address of gene" but "reference to gene"?). It copies from itself: this is a pointer to the current object, so by dereferencing to *this, we are passing the object itself to the copy constructor. See above.
+Bead* Gene::Clone() const    //This function returns a pointer to a bead (a gene in this case). It creates a pointer to a Gene via p_Gene=new Gene(); this is how you can initialise a new pointer to the type Gene. However, it does not call the constructor, because the brackets are not empty. Instead it calls the copy constructor (see above), which by rule requires the reference to a Gene class object (the ampersand does not indicate the "address of gene" but "reference to gene"?). It copies from itself: this is a pointer to the current object, so by dereferencing to *this, we are passing the object itself to the copy constructor. See above.
+{
   return new Gene(*this);   //Note that you have to return a pointer to a Bead type, because Bead is only an abstract type. With the pointer to Bead type that this function makes/returns, you in this case also get the Gene type members the current object is a Gene and so *this is also a Gene. However doing it in this manner means that the function CopyPartOfGenome in Genome.cc can apply to any bead (any class derived from the Bead type). So you generally use pointers to the base class (which drag along their derived class).
 }
