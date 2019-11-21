68507eb67308088248526701f012c230bf382f3b
Sam von der Dunk <samvonderdunk@hotmail.com>
2019-11-21 11:02:44 +0100

    Defined an iterator type for ClaimVectors

--- include/TFBS.hh
+++ include/TFBS.hh
@@ -9,6 +9,8 @@ class TFBS : public Bead {
   int activity; //This could be an element of the basal bead type, but than recoding might be more difficult in the future.
   vector<double>* ClaimVector;   //Each element corresponds to an element in GeneStates and in GeneTypes.
 
+  typedef vector<double>::iterator claim_iter;
+
   TFBS();//constructor
   TFBS(int t, int act, bool bd_dom[]);
   explicit TFBS(const TFBS &tfbs);//copy constructor
